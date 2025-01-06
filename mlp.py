b01283e00eccd31f2f3e33449aa37922059db60c
Shahain Manujith <86903975+itsShahain@users.noreply.github.com>
2025-01-06 17:48:24 +0530

    Added Kaiming initialization and BatchNorm

--- mlp.py
+++ mlp.py
@@ -1,18 +1,18 @@
 """
-Testing loss: ~2.14
+Testing loss: ~2.0156302452087402
 
 Example output:
 
-mariyah
-jailia
-krishaira
-jana
-salas
-elaiyanialace
-jemmalis
-emrey
-karlous
-elisha
+oriuznni
+chh
+itana
+ruhu
+tylah
+emierush
+jabelsentalana
+wrajon
+nisune
+nzili
 """
 
 import torch
@@ -45,36 +45,63 @@ X = torch.tensor(X)
 Y = torch.tensor(Y)
 g = torch.Generator().manual_seed(2147483647)
 
+g = torch.Generator().manual_seed(2147483647)
 C = torch.randn((27, 10), generator=g)
-W1 = torch.randn((30, 200), generator=g) * 1/(150**0.5) # added Kaiming He initialization
+W1 = torch.randn((30, 200), generator=g) * 1/(150**0.5)
 b1 = torch.randn((200,), generator=g) * 0.01
 W2 = torch.randn((200, 27), generator=g) * (5/3)/(150**0.5)
 b2 = torch.randn((27,), generator=g) * 0
 
-parameters = [C, W1, b1, W2, b2]
+bn_gain = torch.ones((1, 200))
+bn_shift = torch.ones((1, 200))
+bn_mean_running = torch.zeros((1, 200))
+bn_std_running = torch.ones((1, 200))
+
+parameters = [C, W1, W2, b2, bn_gain, bn_shift] # left out b1
 
 for p in parameters:
     p.requires_grad = True
 
-parameters = [C, W1, b1, W2, b2]
-
-for epoch in range(50000):
+for epoch in range(10000):
     for p in parameters:
         p.grad = None
+
 
     idx = torch.randint(0, X.shape[0], (32, ))
 
     embedding = C[X[idx]]
-    h = embedding.view(-1, 150) @ W1 + b1
-    logits = torch.tanh(h @ W2 + b2)
+
+    hpreact = embedding.view(-1, 30) @ W1 #+ b1
+    bn_mean_iter = hpreact.mean(dim=0, keepdim=False)
+    bn_std_iter = hpreact.std(dim=0, keepdim=False)
+    hpreact = bn_gain * (hpreact - bn_mean_iter) / (bn_std_iter + 1e-8) + bn_shift
+
+    with torch.no_grad():
+        bn_mean_running = 0.999 * bn_mean_running + 0.001 * bn_mean_iter
+        bn_std_running = 0.999 * bn_std_running + 0.001 * bn_std_iter
+
+    h = torch.tanh(hpreact)
+
+    logits = h @ W2 + b2
     loss = F.cross_entropy(logits, Y[idx])
+
 
     loss.backward()
 
     for p in parameters:
-        p.data = p.data - 0.1 * p.grad
+        p.data = p.data - 0.01 * p.grad
+
 
 print(loss.item())
+
+""" Not needed if maintaining an exponentially weighted average of running mean + std
+with torch.no_grad():
+    embedding = C[X]
+    hpreact = embedding.view(-1, 30) @ W1 + b1
+    bn_mean = hpreact.mean(dim=0, keepdim=False)
+    bn_std = hpreact.std(dim=0, keepdim=False)
+"""
+
 
 context_size = 3
 
@@ -83,8 +110,11 @@ for _ in range(10):
     out = []
     while True:
         embedding = C[torch.tensor(context).view(1, -1)]
-        h = embedding.view(1, -1) @ W1 + b1
-        logits = torch.tanh(h @ W2 + b2)
+        hpreact = embedding.view(-1, 30) @ W1 + b1
+        hpreact = bn_gain * ((hpreact - bn_mean_running) / (bn_std_running + 0.01)) + bn_shift
+        h = torch.tanh(hpreact)
+
+        logits = h @ W2 + b2
         probs = F.softmax(logits, dim=1)
         idx = torch.multinomial(probs[0], num_samples=1, generator=g).item()
         if idx == 0:
